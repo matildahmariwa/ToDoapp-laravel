816742f0f3966a63a22ceb22455649097b042c9a
unknown <matildamariwa3@gmail.com>
2019-09-02 11:24:27 +0300

    modal buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- <div class="container">
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div> --}}
+                <div class="card-header">My tasks</div>
 
                 <div class="panel-body">
                         
-                    <p>My tasks</p>
-    {!! Form::open(['action' => 'TasksController@store','method'=>'POST','enctype'=>'multipart/form-data'])!!}
-        <div class="form-group">
-        {{Form::label('item','Create task')}}
-        {{Form::text('item','',['class'=>'form-control','placeholder'=>'Insert here'])}}
+                   
+    
+        <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" style="margin-left:5px">
+       Create task
+      </button>
+      
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Create</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                    {!! Form::open(['action' => 'TasksController@store','method'=>'POST','enctype'=>'multipart/form-data'])!!}
+                    <div class="form-group">
+                    {{Form::label('item','Create task')}}
+                    {{Form::text('item','',['class'=>'form-control','placeholder'=>'Insert here'])}}
+                    </div>
+                    {{Form::submit('submit',['class'=>'btn btn-primary','type'=>'submit','id'=>'submit'])}}
+                    
+                    {!! Form::close() !!}
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+             
+            </div>
+          </div>
         </div>
-        {{Form::submit('submit',['class'=>'btn btn-primary','type'=>'submit','id'=>'submit'])}}
-        
-        {!! Form::close() !!}
+      </div>
         @include('inc.messages') 
                  
-                    <div class="panel panel-default ">
-                        <div class="panel-heading">
+                    <div>
+                        <div >
                             My tasks today
                         </div>
                         <div class="panel-body">
@@ -38,7 +63,7 @@
                                 <td class="table-text">
                                 <div>{{$task->item}}</div>
                                 </td>
-
+                              <div>
                                 <td>{!!Form::open(['action'=>['TasksController@destroy',$task->id],'method'=>'POST','class'=>'pull-right'])!!}
                                         {{Form::hidden('_method','DELETE')}}
                                         {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
@@ -47,6 +72,7 @@
                                {{-- <a href=""></a> --}}
                                <a href="/To-do/public/home/{{$task->id}}/edit" class="btn btn-default" style="float:left">Edit</a>
                            <td>
+                           </div>
                             </tr>
                             @endforeach
                            
